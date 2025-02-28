3657b99ac7488354bfe84b12a750c02977d79652
MikkelSchmidtNielsen <mikkelschmidt@msn.com>
2025-02-28 12:33:01 +0100

    Linked services classes ProduceService and UserProduce to their respective interfaces.

--- Services/ProduceService.cs
+++ Services/ProduceService.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Interfaces;
+using Domain.Models;
 
 namespace Services
 {
-    internal class ProduceService : IProduceService
-    {
-    }
+	internal class ProduceService : IProduceService
+	{
+		public IEnumerable<Produce> GetAllProduce()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Produce> GetMatchesByName(string input)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Produce GetProduceByID(int produceID)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
--- Services/UserProduce.cs
+++ Services/UserProduce.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Interfaces;
+using Domain.Models;
 
 namespace Services
 {
-    internal class UserProduce : IUserProduce
-    {
-    }
+	internal class UserProduce : IUserProduce
+	{
+		public void AddProduce(Produce newProduce)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void DeleteProduce(Produce produce)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void FindRecipes(List<string> UserProduce)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
