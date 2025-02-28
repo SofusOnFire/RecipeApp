b89d9943b441aebb0f1778c291575e620aa3e68c
MikkelSchmidtNielsen <mikkelschmidt@msn.com>
2025-02-28 17:14:14 +0100

    Implemented GetProduceByID method to Repository

--- Dal/ProduceRepository.cs
+++ Dal/ProduceRepository.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Interfaces;
+using Domain.Models;
+using Microsoft.Data.SqlClient;
 
 namespace DAL
 {
-    class ProduceRepository
+    public class ProduceRepository
     {
+        public IEnumerable<Produce> GetProduceByID(int produceID)
+        {
+			List<Produce> list = new List<Produce>();
+
+			return list;
+		}
     }
 }
