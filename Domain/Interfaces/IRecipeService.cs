d9a5d809724ed1764c4be3b9437b0cbf3285a241
MikkelSchmidtNielsen <mikkelschmidt@msn.com>
2025-02-28 12:26:24 +0100

    Created IRecipeService with methods. Created RecipeService. Implemented methods RecipeService from interface IRecipeService

---
+++ Domain/Interfaces/IRecipeService.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IRecipeService
+    {
+        /// <summary>
+        /// Gets all recipes from the database.
+        /// </summary>
+        /// <returns>Returns a list of recipes</returns>
+        public IEnumerable<Recipe> GetAllRecipesFromDatabase();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="recipeID"></param>
+        /// <returns></returns>
+        public IEnumerable<ProduceLine> GetAllRecipeProduceLines(int recipeID);
+
+        /// <summary>
+        /// Gets all recipes based on the users producelist.
+        /// </summary>
+        /// <param name="produceList"></param>
+        /// <returns>Returns a list with matching recipes</returns>
+        public IEnumerable<Recipe> GetAllByProduce(IUserProduceService produceList);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="produceLines"></param>
+        public void SetProduce(IEnumerable<ProduceLine> produceLines);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userProduce"></param>
+        /// <param name="recipes"></param>
+        /// <returns></returns>
+        public IEnumerable<Recipe> FindRecipes(IEnumerable<string> userProduce, IEnumerable<Recipe> recipes);
+    }
+}
---
+++ Services/RecipeService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+using Domain.Models;
+
+namespace Services
+{
+	class RecipeService : IRecipeService
+	{
+		private readonly IUserProduceService _userProduceService; // DI for FindRecipe method.
+
+		public IEnumerable<Recipe> FindRecipes(IEnumerable<string> userProduce, IEnumerable<Recipe> recipes)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Recipe> GetAllByProduce(IUserProduceService produceList)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<ProduceLine> GetAllRecipeProduceLines(int recipeID)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Recipe> GetAllRecipesFromDatabase()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SetProduce(IEnumerable<ProduceLine> produceLines)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
