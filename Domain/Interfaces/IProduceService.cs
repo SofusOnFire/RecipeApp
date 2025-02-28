9141d9b5521d51ae741ee85f653a9052292d70e7
Sarah <peasblinky@gmail.com>
2025-02-28 11:15:35 +0100

    Added IProduceService.cs, IUserProduce.cs, ProduceService.cs and UserProduce.cs

---
+++ Domain/Interfaces/IProduceService.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IProduceService
+    {
+        public IEnumerable<Produce> GetAll();
+
+        public Produce GetByProduceID(Produce produce);
+
+        public IEnumerable<Produce> GetMatchesByName(string input);
+
+    }
+}
