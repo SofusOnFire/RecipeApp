279ef475fdedd3cd3cbec56dfabf1b6f201ec9f2
MikkelSchmidtNielsen <mikkelschmidt@msn.com>
2025-02-28 12:27:57 +0100

    Made Recipe into public class

--- Domain/Models/Recipe.cs
+++ Domain/Models/Recipe.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain.Models
 {
-    internal class Recipe
+    public class Recipe
     {
         public int? RecipeID { get; private set; }
         public string? Name { get; private set; }
