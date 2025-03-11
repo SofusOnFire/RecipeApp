1f3d7286cfddaf89ce61bbc09d76fe2b0f3fba3a
MikkelRLarsen <Thenuker95@gmail.com>
2025-03-11 09:02:39 +0100

    DbcontextEntityFramework

---
+++ Dal/DbcontextEntityFramework.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace DAL
+{
+	internal class DbcontextEntityFramework : DbContext
+	{
+		public DbSet<Recipe> Recipes { get; set; }
+		public DbSet<ProduceLine> ProduceLines { get; set; }
+		public DbSet<Produce> Produces { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(GetConnectionString());
+		}
+
+		protected static string GetConnectionString()
+		{
+			string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+			string fullName = Path.Combine(desktopPath, "RecipeAppConnectionString");
+			return File.ReadAllText(fullName);
+		}
+	}
+}
---
+++ Dal/ProduceRepositoryEntityFramework.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+using Domain.Models;
+
+namespace DAL
+{
+	internal class ProduceRepositoryEntityFramework : IProduceRepository
+	{
+		private readonly DbcontextEntityFramework _dbcontext = new DbcontextEntityFramework();
+
+		public List<Produce> GetAllProduce()
+		{
+			return _dbcontext.Produces.ToList();
+		}
+
+		public Produce GetProduceByID(int produceID)
+		{
+			return _dbcontext.Produces.Where(p => p.ProduceID == produceID).FirstOrDefault();
+		}
+	}
+}
