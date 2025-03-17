505542d60c5a98596a431ebead2c2184168793e0
Sarah <peasblinky@gmail.com>
2025-03-17 14:02:43 +0100

    added comments to the CompareAndGetProduceList method

--- Services/ProduceService.cs
+++ Services/ProduceService.cs
@@ -22,6 +22,16 @@ namespace Services
 			return _produceRepository.GetProduceByID(produceID);
 		}
 
+
+		/* This method creates a new produce list that only contains the
+		 * produces that aren't in the users personal list of produces. 
+		 * 
+		 * If the users produce list isn't empty, it'll iterate through all produces
+		 * in the database. If a produce doesn't already exist on the users
+		 * produce list, it'll be added to the produce list.
+		 * After going through all the produces on the users list, it'll
+		 * return a list of produces that aren't already on the user produce list.
+		 * If the users produce list is empty, a list of all produces will be returned. */
 		public List<Produce> CompareAndGetProduceList(List<Produce> userProduces)
 		{
 			List<Produce> produceList = new List<Produce>();
