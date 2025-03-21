9a955d4474d5834a0fadde2c8ef62c93c92ee07a
MikkelRLarsen <Thenuker95@gmail.com>
2025-03-21 10:09:14 +0100

    Added DocBlock for IUnitRepository

--- Domain/Interfaces/IUnitRepositry.cs
+++ Domain/Interfaces/IUnitRepositry.cs
@@ -9,7 +9,17 @@ namespace Domain.Interfaces
 {
 	public interface IUnitRepositry
 	{
+		/// <summary>
+		/// Retrieves a unit based on the provided unit ID.
+		/// </summary>
+		/// <param name="unitID">The unique identifier of the unit.</param>
+		/// <returns>The <see cref="Unit"/> object if found; otherwise, null.</returns>
 		public Unit GetUnitByUnitID(int unitID);
+
+		/// <summary>
+		/// Retrieves all available units.
+		/// </summary>
+		/// <returns>An <see cref="IEnumerable{T}"/> of <see cref="Unit"/> objects.</returns>
 		public IEnumerable<Unit> GetAllUnits();
 	}
 }
--- Test/ModelsTest/ProduceTest.cs
+++ Test/ModelsTest/ProduceTest.cs
@@ -4,23 +4,11 @@ using Domain.Models;
 [TestClass]
 public class ProduceTests
 {
-	private Produce _testProduce;
-	private List<int> _testAmountNeededForRecipe;
-
-	[TestInitialize]
-	public void Setup()
-	{
-		// Initialize lists before adding elements
-		_testProduce = new Produce(1, "Tomat", 1);
-
-		_testAmountNeededForRecipe = new List<int> { 0, -1, 100, 1, 100 };
-	}
-
 	[TestMethod]
 	public void SetStockStatus_ShouldChangeStatusColorOfProduce_WhenProduceAndNeededAmountIsPassedIntoMethod()
 	{
 		// Arrange
-		var produce = _testProduce; // Tomat, UserAmount = 1
+		var produce = new Produce(1, "Tomat", 1); //UserAmount = 1
 		var comparedProduce = new Produce(1, "Tomat", 2);
 
 		// Act & Assert
@@ -31,17 +19,17 @@ public class ProduceTests
 
 		// Case 2: comparedProduce.UserAmount == 0 -> Should be "red"
 		comparedProduce.UserAmount = 0;
-		produce.SetStockStatus(comparedProduce, _testAmountNeededForRecipe[0]); // 0
+		produce.SetStockStatus(comparedProduce, 0); // 0
 		Assert.AreEqual("red", produce.InStockStatus, "Expected 'red' when comparedProduce.UserAmount is 0");
 
 		// Case 3: recipeAmountOfIngredient <= comparedProduce.UserAmount -> Should be "green"
 		comparedProduce.UserAmount = 100;
-		produce.SetStockStatus(comparedProduce, _testAmountNeededForRecipe[2]); // 100
+		produce.SetStockStatus(comparedProduce, 100); // 100
 		Assert.AreEqual("green", produce.InStockStatus, "Expected 'green' when recipe amount is less than or equal to stock");
 
 		// Case 4: recipeAmountOfIngredient > comparedProduce.UserAmount -> Should be "yellow"
 		comparedProduce.UserAmount = 1;
-		produce.SetStockStatus(comparedProduce, _testAmountNeededForRecipe[2]); // 100 (greater than 1)
+		produce.SetStockStatus(comparedProduce, 100); // 100 (greater than 1)
 		Assert.AreEqual("yellow", produce.InStockStatus, "Expected 'yellow' when recipe amount is greater than stock");
 	}
 }
